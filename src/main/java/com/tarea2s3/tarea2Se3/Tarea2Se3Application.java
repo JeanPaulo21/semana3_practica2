54a9dffc9e70d6af4a9b01d9d9ba505907c016ae
JeanPaulo21 <126036883+JeanPaulo21@users.noreply.github.com>
2024-06-13 12:41:03 -0600

    semana3-practica evaluativa

---
+++ src/main/java/com/tarea2s3/tarea2Se3/Tarea2Se3Application.java
@@ -0,0 +1,13 @@
+package com.tarea2s3.tarea2Se3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2Se3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2Se3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea2s3/tarea2Se3/Tarea2Se3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea2s3.tarea2Se3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2Se3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
